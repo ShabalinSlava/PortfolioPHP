4205ffbce2b3afd066c1829e5bf0bbefa38883e2
Shabalin Slava <svaznoy0309@gmail.com>
2020-08-10 22:28:28 +0800

    added data file

---
+++ data.php
@@ -0,0 +1,106 @@
+<?php
+return [
+  'about' => [
+    'name' => 'Ivan Ivanov',
+    'post' => 'Manager',
+    'email' => 'aaa@gmail.com',
+    'phone' => '+839329820',
+    'site' => 'google.com',
+    'linkedin' => 'linkedin.com',
+    'github' => 'github.com',
+    'twitter' => 'twitter.com'
+  ],
+  'education' => [
+    [
+      'faculty' => 'Random faculty',
+      'university' => 'Smth University',
+      'yearStart' => 2015,
+      'yearEnd' => 2018
+    ],
+    [
+      'faculty' => 'Random faculty 2',
+      'university' => 'Smth University 2',
+      'yearStart' => 2017,
+      'yearEnd' => 2020
+    ]
+  ],
+  'languages' => [
+    'English', 'Russian', 'Germany'
+  ],
+  'interests' => [
+    'interest 1', 'interest 2'
+  ],
+  'aboutCareer' => 'Here begint a text',
+  'career' => [
+    [
+      'post' => 'post 1',
+      'place' => 'City 1',
+      'duty' => 'you must lorem 1',
+      'yearStart' => 2015,
+      'yearEnd' => 'Present'
+    ],
+    [
+      'post' => 'post 2',
+      'place' => 'City 2',
+      'duty' => 'you must lorem 2',
+      'yearStart' => 2016,
+      'yearEnd' => 2018
+    ],
+    [
+      'post' => 'post 3',
+      'place' => 'City 3',
+      'duty' => 'you must lorem 3',
+      'yearStart' => 2017,
+      'yearEnd' => 2019
+    ],
+  ],
+  'projects' => [
+      'intro' => 'Here you can to write your projects',
+    [
+      'company' => 'company 1',
+      'description' => 'description 1'
+    ],
+    [
+      'company' => 'company 2',
+      'description' => 'description 2'
+    ],
+    [
+      'company' => 'company 3',
+      'description' => 'description 3'
+    ],
+    [
+      'company' => 'company 4',
+      'description' => 'description 4'
+    ],
+    [
+      'company' => 'company 5',
+      'description' => 'description 5'
+    ],
+  ],
+  'skills' => [
+    [
+      'skill' => 'Python',
+      'level' => '98%'
+    ],
+    [
+      'skill' => 'JavaScript',
+      'level' => '98%'
+    ],
+    [
+      'skill' => 'React',
+      'level' => '50%'
+    ],
+    [
+      'skill' => 'Vue',
+      'level' => '70%'
+    ],
+    [
+      'skill' => 'Ruby',
+      'level' => '20%'
+    ],
+    [
+      'skill' => 'Sketch',
+      'level' => '45%'
+    ],
+  ]
+];
